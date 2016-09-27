6a64b4a6c10dde28b0df1c1cdba1b9744cd79bc8
Jake Reher <jake.reher@gmail.com>
2016-09-26 21:46:58 -0400

    Added separate folder for third party Matlab packages.

--- matlab/matlab_utilities/matlab_utilities_depends.m
+++
@@ -1,7 +0,0 @@
-%> @brief Forward functionality of addpath_matlab_utilites() to maintain compatibility
-%> @author Eric Cousineau
-function [] = addpath_matlab_utilities(varargin)
-
-addpath_matlab_utilities(varargin{:});
-
-end
