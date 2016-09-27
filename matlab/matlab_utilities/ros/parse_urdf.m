021c43719846c356554b58122818e3c3bdcd302c
Jake Reher <jake.reher@gmail.com>
2016-09-27 00:34:18 -0400

    script for setup default path for dependent packages.

---
+++ matlab/matlab_utilities/ros/parse_urdf.m
@@ -0,0 +1,20 @@
+function robot = parse_urdf(urdf_file)
+% This function parse the ROS URDF file into a matlab struct
+%
+% @todo This function need to be implemented soon 
+%
+% Parameters:
+% urdf_file: the URDF file full path @char
+% 
+% Return values:
+% robot: a matlab struct that contains parsed robot parameters from the
+% URDF file.
+
+robot = [];
+error('Cannot support URDF file type at this moment.');
+
+
+
+
+end
+
---
+++ matlab/setup_default_path.m
@@ -0,0 +1,19 @@
+function setup_default_path()
+% Script that setup all the environment variables for the project
+
+% Add system related functions paths
+cur = [fileparts(mfilename('fullpath')), '/'];
+
+
+
+% Add useful custom functions path
+addpath([cur, 'matlab_utilities/']);
+addpath_matlab_utilities('general', ...
+    'graphics', 'cwf', 'polynomial', 'sys', ...
+    'ros', 'sim', 'plot', 'validation');
+
+% Add third party packages/libraries path
+addpath([cur, 'third/']);
+addpath_thirdparty_packages('GetFullPath',...
+    'sparse2', 'strings', 'yaml', 'spatial_v2');
+end
