8a923771a1c58c8c46ac4c6e8e04223cdf136048
Ayonga Hereid <ayonga27@gmail.com>
2016-11-05 11:04:13 +0800

    Move 'includes' folder outside of examples.

---
+++ include/mdefs.hpp
@@ -0,0 +1,33 @@
+/**
+ * \brief Modified Mathematica Definitions file
+ * \author Wolfram Research Inc., Copyright 1986 through 1999
+ * \author Eric Cousineau - Modified because macros suck.
+ */
+
+inline double Power(double x, double y) { return pow(x, y); }
+inline double Sqrt(double x) { return sqrt(x); }
+
+inline double Abs(double x) { return fabs(x); }
+
+inline double Exp(double x) { return exp(x); }
+inline double Log(double x) { return log(x); }
+
+inline double Sin(double x) { return sin(x); }
+inline double Cos(double x) { return cos(x); }
+inline double Tan(double x) { return tan(x); }
+
+inline double ArcSin(double x) { return asin(x); }
+inline double ArcCos(double x) { return acos(x); }
+inline double ArcTan(double x) { return atan(x); }
+
+/* update ArcTan function to use atan2 instead. */
+inline double ArcTan(double x, double y) { return atan2(y,x); }
+
+inline double Sinh(double x) { return sinh(x); }
+inline double Cosh(double x) { return cosh(x); }
+inline double Tanh(double x) { return tanh(x); }
+
+const double E	= 2.71828182845904523536029;
+const double Pi = 3.14159265358979323846264;
+const double Degree = 0.01745329251994329576924;
+
