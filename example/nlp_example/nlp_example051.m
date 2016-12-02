fff521c77c287beb3423dbb04954814e58b3c9c8
Ayonga Hereid <ayonga27@gmail.com>
2016-12-02 02:44:52 -0500

    updated simple nlp exmple based on the current changes.

--- example/nlp_example/nlp_example051.m
+++ example/nlp_example/nlp_example051.m
@@ -3,33 +3,89 @@ addpath('export/');
 
 setup_default_path;
 
-nlp = NonlinearProgram('ex051','withHessian',true);
 
-nlp = addVariable(nlp,'x',3);
-nlp = addVariable(nlp,'y',2);
+options.derivative_level = 1;
+nlp = NonlinearProgram(options);
+
+
+x = NlpVariable('x',3,-inf, inf, [2.5 0.5 2]);
+y = NlpVariable('y',2,-inf, inf, [-1 0.5]);
+nlp = addVariable(nlp,x);
+nlp = addVariable(nlp,y);
 
 nlp = genVarIndices(nlp);
 
-nlp = addCost(nlp,'cost1',{'x'});
-nlp = addCost(nlp,'cost2',{'x'});
-nlp = addCost(nlp,'cost3',{'y'});
+costs(1) = NlpFunction('f_cost1','nonlinear');
+costs(1) = setDependent(costs(1),{x});
+js = feval('Js_cost1',0);
+costs(1) = setJacobianPattern(costs(1),js,'IndexForm');
+costs(1) = setJacobianFunction(costs(1),'J_cost1');
+hs = feval('Hs_cost1',0);
+costs(1) = setHessianPattern(costs(1),hs,'IndexForm');
+costs(1) = setHessianFunction(costs(1),'H_cost1');
 
+costs(2) = NlpFunction('f_cost2','nonlinear');
+costs(2) = setDependent(costs(2),{x});
+js = feval('Js_cost2',0);
+costs(2) = setJacobianPattern(costs(2),js,'IndexForm');
+costs(2) = setJacobianFunction(costs(2),'J_cost2');
+hs = feval('Hs_cost2',0);
+costs(2) = setHessianPattern(costs(2),hs,'IndexForm');
+costs(2) = setHessianFunction(costs(2),'H_cost2');
 
-nlp = addConstraint(nlp,'constr1',{'x'},1,4,4);
-nlp = addConstraint(nlp,'constr2',{'x','y'},1,0,0);
-nlp = addConstraint(nlp,'constr3',{'x','y'},1,0,0);
+costs(3) = NlpFunction('f_cost3','nonlinear');
+costs(3) = setDependent(costs(3),{y});
+js = feval('Js_cost3',0);
+costs(3) = setJacobianPattern(costs(3),js,'IndexForm');
+costs(3) = setJacobianFunction(costs(3),'J_cost3');
+hs = feval('Hs_cost3',0);
+costs(3) = setHessianPattern(costs(3),hs,'IndexForm');
+costs(3) = setHessianFunction(costs(3),'H_cost3');
 
+nlp = addObjective(nlp,costs);
 
-nlp = setInitialGuess(nlp,[ 2.5 0.5 2 -1 0.5 ]);
+constrs(1) = NlpFunction('f_constr1','nonlinear');
+constrs(1) = setDependent(constrs(1),{x});
+js = feval('Js_constr1',0);
+constrs(1) = setJacobianPattern(constrs(1),js,'IndexForm');
+constrs(1) = setJacobianFunction(constrs(1),'J_constr1');
+hs = feval('Hs_constr1',0);
+constrs(1) = setHessianPattern(constrs(1),hs,'IndexForm');
+constrs(1) = setHessianFunction(constrs(1),'H_constr1');
+constrs(1) = setBoundaryValue(constrs(1),4,4);
+
+constrs(2) = NlpFunction('f_constr2','nonlinear');
+constrs(2) = setDependent(constrs(2),{x,y});
+js = feval('Js_constr2',0);
+constrs(2) = setJacobianPattern(constrs(2),js,'IndexForm');
+constrs(2) = setJacobianFunction(constrs(2),'J_constr2');
+hs = feval('Hs_constr2',0);
+constrs(2) = setHessianPattern(constrs(2),hs,'IndexForm');
+constrs(2) = setHessianFunction(constrs(2),'H_constr2');
+constrs(2) = setBoundaryValue(constrs(2),0,0);
+
+constrs(3) = NlpFunction('f_constr3','nonlinear');
+constrs(3) = setDependent(constrs(3),{x,y});
+js = feval('Js_constr3',0);
+constrs(3) = setJacobianPattern(constrs(3),js,'IndexForm');
+constrs(3) = setJacobianFunction(constrs(3),'J_constr3');
+hs = feval('Hs_constr3',0);
+constrs(3) = setHessianPattern(constrs(3),hs,'IndexForm');
+constrs(3) = setHessianFunction(constrs(3),'H_constr3');
+constrs(3) = setBoundaryValue(constrs(3),0,0);
+
+nlp = addConstraint(nlp,constrs);
 
 
 extraOpts.fixed_variable_treatment = 'RELAX_BOUNDS';
 extraOpts.point_perturbation_radius = 0;
-extraOpts.derivative_test = 'second-order';
-extraOpts.derivative_test_perturbation = 1e-8;
+extraOpts.jac_c_constant        = 'yes';
+extraOpts.hessian_approximation = 'limited-memory';
+extraOpts.derivative_test = 'first-order';
+extraOpts.derivative_test_perturbation = 1e-7;
 
-solver = IpoptApplication(nlp, extraOpts);
+solverApp = IpoptApplication(nlp, extraOpts);
 
-solver = initialize(solver);
 
-solver = solver.optimize
+
+[sol, info] = optimize(solverApp, nlp);
