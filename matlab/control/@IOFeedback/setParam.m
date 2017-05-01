422cc55852c39d7afd5803c17bc95aa3c0b23a3f
Ayonga Hereid <ayonga27@gmail.com>
2017-05-01 14:41:36 -0400

    Updated the IOFeedback control class to cope with the new implementation.

--- matlab/control/@IOFeedback/setParam.m
+++ matlab/control/@IOFeedback/setParam.m
@@ -16,26 +16,26 @@ function obj = setParam(obj, varargin)
     % the same time. In this case, only ''ep'' will be used.
 
 
-    argin = struct(varargin{:});
-
-    if isfield(argin, 'ep')
-        assert(isnumeric(argin.ep) && isscalar(argin.ep) && (argin.ep > 0), ...
-            ['(epsilon) must be a scalar positive number. \n',...
-            'wrong input argument: %d'], argin.ep);
-        obj.Param.kp = argin.ep^2;
-        obj.Param.kd = 2*argin.ep;
-    else
-        if isfield(argin, 'kp')
-            assert(isnumeric(argin.kp) && isscalar(argin.kp) && (argin.kp > 0), ...
-                ['(kp) must be a scalar positive number. \n',...
-                'wrong input argument: %d'], argin.kp);
-            obj.Param.kp = argin.kp;
-        end
-        if isfield(argin, 'kd')
-            assert(isnumeric(argin.kd) && isscalar(argin.kd) && (argin.kd > 0), ...
-                ['(kd) must be a scalar positive number. \n',...
-                'wrong input argument: %d'], argin.kd);
-            obj.Param.kd = argin.kd;
-        end
-    end
+    %     argin = struct(varargin{:});
+    %
+    %     if isfield(argin, 'ep')
+    %         assert(isnumeric(argin.ep) && isscalar(argin.ep) && (argin.ep > 0), ...
+    %             ['(epsilon) must be a scalar positive number. \n',...
+    %             'wrong input argument: %d'], argin.ep);
+    %         obj.Param.kp = argin.ep^2;
+    %         obj.Param.kd = 2*argin.ep;
+    %     else
+    %         if isfield(argin, 'kp')
+    %             assert(isnumeric(argin.kp) && isscalar(argin.kp) && (argin.kp > 0), ...
+    %                 ['(kp) must be a scalar positive number. \n',...
+    %                 'wrong input argument: %d'], argin.kp);
+    %             obj.Param.kp = argin.kp;
+    %         end
+    %         if isfield(argin, 'kd')
+    %             assert(isnumeric(argin.kd) && isscalar(argin.kd) && (argin.kd > 0), ...
+    %                 ['(kd) must be a scalar positive number. \n',...
+    %                 'wrong input argument: %d'], argin.kd);
+    %             obj.Param.kd = argin.kd;
+    %         end
+    %     end
 end
