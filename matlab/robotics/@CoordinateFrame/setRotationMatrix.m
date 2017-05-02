12afb6d6736b98f1edeb2a16ae011897def5e91a
Jake Reher <jake.reher@gmail.com>
2017-05-02 11:41:44 -0700

    Make rotx/y/z independent of Phased Array System Toolbox

--- matlab/robotics/@CoordinateFrame/setRotationMatrix.m
+++ matlab/robotics/@CoordinateFrame/setRotationMatrix.m
@@ -18,3 +18,45 @@ function obj = setRotationMatrix(obj,r)
     % update the homogeneous transformation matrix
     obj.computeHomogeneousTransform();
 end
+
+function R = rotx(alpha)
+%rotz  rotate around X by ALPHA
+%
+%	R = rotx(alpha)
+%
+% See also: roty, rotz
+% Author: Jake Reher: jreher@caltech.edu
+
+R = [1 0 0; ...
+     0 cos(alpha) -sin(alpha); ...
+     0 sin(alpha)  cos(alpha)];
+          
+end
+
+function R = roty(alpha)
+%roty  rotate around Y by ALPHA
+%
+%	R = roty(alpha)
+%
+% See also: rotx, rotz
+% Author: Jake Reher: jreher@caltech.edu
+
+R = [cos(alpha)  0 sin(alpha); ...
+             0   1          0; ...
+    -sin(alpha)  0 cos(alpha)];
+          
+end
+
+function R = rotz(alpha)
+%rotz  rotate around Z by ALPHA
+%
+%	R = rotz(alpha)
+%
+% See also: ROTX, ROTY, ROT, POS.
+% Author: Jake Reher: jreher@caltech.edus
+
+R = [cos(alpha) -sin(alpha) 0; ...
+     sin(alpha)  cos(alpha) 0; ...
+              0           0 1];
+
+end
