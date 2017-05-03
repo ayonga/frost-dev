adda03e7926a47fca574717d726aaf7568a62e22
Ayonga Hereid <ayonga27@gmail.com>
2017-05-03 01:04:33 -0400

    Fixed the sign of drift vectors for RobotLinks.

--- matlab/robotics/@RobotLinks/configureDynamics.m
+++ matlab/robotics/@RobotLinks/configureDynamics.m
@@ -28,18 +28,21 @@ function obj = configureDynamics(obj)
     Qe = obj.States.x;
     dQe = obj.States.dx;
     
+    %|@note !!! The minus sign !!!
+    % Fvec appears at the right hand side (typically C(q,dq)dq + G(q)
+    % appears at the left hand side of the Euler-Lagrangian Equation)
     Ce1 = cell(obj.numState,1);
     Ce2 = cell(obj.numState,1);
     Ce3 = cell(obj.numState,1);
     for i=1:obj.numState
-        Ce1{i} = SymFunction(['Ce1_vec',num2str(i),'_',obj.Name],eval_math_fun('InertiaToCoriolisPart1',{De,Qe,dQe,i}),{Qe,dQe});
-        Ce2{i} = SymFunction(['Ce2_vec',num2str(i),'_',obj.Name],eval_math_fun('InertiaToCoriolisPart2',{De,Qe,dQe,i}),{Qe,dQe});
-        Ce3{i} = SymFunction(['Ce3_vec',num2str(i),'_',obj.Name],eval_math_fun('InertiaToCoriolisPart3',{De,Qe,dQe,i}),{Qe,dQe});
+        Ce1{i} = SymFunction(['Ce1_vec',num2str(i),'_',obj.Name],-eval_math_fun('InertiaToCoriolisPart1',{De,Qe,dQe,i}),{Qe,dQe});
+        Ce2{i} = SymFunction(['Ce2_vec',num2str(i),'_',obj.Name],-eval_math_fun('InertiaToCoriolisPart2',{De,Qe,dQe,i}),{Qe,dQe});
+        Ce3{i} = SymFunction(['Ce3_vec',num2str(i),'_',obj.Name],-eval_math_fun('InertiaToCoriolisPart3',{De,Qe,dQe,i}),{Qe,dQe});
     end
     
     % We add dQe as dependent variables to the gravity vector in order to have a same structure
     % as the corilios forces
-    Ge = SymFunction(['Ge_vec_',obj.Name],eval_math_fun('GravityVector',[links,{Qe}]),{Qe,dQe});
+    Ge = SymFunction(['Ge_vec_',obj.Name],-eval_math_fun('GravityVector',[links,{Qe}]),{Qe,dQe});
     
     vf = [Ce1;Ce2;Ce3;{Ge}];
     
