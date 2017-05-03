8d78bb5000f4236fbbc2c9658e15f41704376041
Ayonga Hereid <ayonga27@gmail.com>
2017-05-03 15:31:12 -0400

    FLippy simulation.

--- example/flippy/main_sim.m
+++ example/flippy/main_sim.m
@@ -15,19 +15,93 @@ end
 %%%% FLIPPY robot model object
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 urdf_file = fullfile(cur,'urdf','robot_only_arm_noeff.urdf');
-flippy = Flippy(urdf_file);
+%@note you could directly create an object, instead of making new subclass.
+base = get_base_dofs('fixed');
+flippy = RobotLinks(urdf_file,base);
+flippy.configureDynamics();
 
+% holonomic constraints??? 
+% obj.addHolonomicConstraints(constr);
+
+
+% contacts???
+% wrist_3_link = obj.Links(getLinkIndices(obj, 'wrist_3_link'));
+% wrist_3_frame = wrist_3_link.Reference;
+% % wrist_3_joint = obj.Joints(getJointIndices(obj, 'r_leg_akx'));
+% EndEff = ContactFrame(...
+%     'Name','EndEff',...
+%     'Reference',wrist_3_frame,...
+%     'Offset',[0, 0, 0],...
+%     'Type',PlanarContactWithFriction,...
+%     'R',[0,0,0]... % z-axis is the normal axis, so no rotation required
+%     );
+% flippy.addContact(EndEff);
+
+
+% unilateral constraints???
+q = flippy.States.x; % symbolic variable for joint configuration
+q_wr = q('wrist_3_joint'); % waist roll joint. The following two referencing both works: q('wrist_3_joint') == q(6);
+delta_final =  q_wr - pi; % 
+u_delta_final = UnilateralConstraint(flippy,delta_final,'deltafinal','x'); %'x' represents joint configuration here, the delta_final is function of x
+
+% unilateral constraints will be automatically imposed as path constraint
+% in the trajectory optimization. They won't affects the simulation
+% process.
+% addUnilateralConstraints(obj, constr)
+%
+% If wants to use unilateral constraints to trigger events in simulation,
+% add them as event functions
+% addEvent(obj, event)
+addEvent(flippy, u_delta_final);
+
+
+% % virtual constraints???
+% tau
+p = SymVariable('p',[2,1]);
+tau = (q_wr-p(2))/(p(1)-p(2));
+
+% y1 = dq_wr
+ya1 = jacobian(q_wr, flippy.States.x)*flippy.States.dx;
+y1_name = 'vel';
+y1 = VirtualConstraint(flippy, ya1, y1_name,...
+    'DesiredType','Constant',...
+    'RelativeDegree',1,'OutputLabel',{'WristRoll'},'PhaseType','StateBased',...
+    'PhaseVariable',tau,'PhaseParams',p,'Holonomic',false);
+
+% % y2 
+% obj = addPositionOutput(obj, ...
+%                 {model.KinObjects.ShoulderPan,...
+%                 model.KinObjects.ShoulderLift,...
+%                 model.KinObjects.Elbow,...
+%                 model.KinObjects.WristYaw,...
+%                 model.KinObjects.WristPitch}, ...
+%                 'Bezier6thOrder');
+y_sp = q('shoulder_pan_joint');
+y_sl = q('shoulder_lift_joint');
+y_el = q('elbow_joint');
+y_wy = q('wrist_2_joint');
+y_wp = q('wrist_1_joint');
+ya_2 = [y_sp; y_sl; y_el; y_wy; y_wp];
+    
+y2_label = {'ShoulderPan',...
+    'ShoulderLift',...
+    'Elbow',...
+    'WristYaw',...
+    'WristPitch'};
+y2_name = 'pos';
+y2 = VirtualConstraint(flippy, ya_2, y2_name,...
+    'DesiredType','Bezier','PolyDegree',6,...
+    'RelativeDegree',2,'OutputLabel',{y2_label},'PhaseType','StateBased',...
+    'PhaseVariable',tau,'PhaseParams',p,'Holonomic',true);
+flippy = addVirtualConstraint(flippy,y1);
+flippy = addVirtualConstraint(flippy,y2);
+
+io_control  = IOFeedback('IO');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Compile and export model specific functions
 %%%% (uncomment the following lines when run it for the first time.)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  flippy.initialize();
-%  compileCoM(flippy);
-%  compileDynamics(flippy);
-% exportCoM(flippy, export_path, true);
-% exportDynamics(flippy, export_path, true);
-% exportLineFunctions(flippy, export_path)
-
+flippy.compile(export_path);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Model for the movement of FLIPPY from point A to point B
