c2020588811cb633b8bef245fe79e6d8d88f2846
Shishir Kolathaya <shishirny@gmail.com>
2017-05-26 17:40:57 -0700

    Working trajectory. This is from the optimization. Only scoopinng for now.

--- example/flippy/custom/simplespatulaplanner.m
+++ example/flippy/custom/simplespatulaplanner.m
@@ -4,39 +4,39 @@ function result = simplespatulaplanner
 
 %% some specifications
 num_node = 30;    % number of nodes
-mu = 0.33; % coefficient of restitution
+mu = 0.17; % coefficient of restitution
 g = 9.8; % gravity
 wrist3joint_length = 0.09465;
 % distance from mid point of spatula to its edge in x direction
 spatula_depth = 0.06; 
-t_min = 0.2;
-t_max = 0.6;
+t_min = 0.3;
+t_max = 0.3;
 n_outputs = 5;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% initial guess
 
-  x0 = [0.6          -0.7350         0    0.5545         0    0.0257 ... 
+  x0 = [0.3      -16.4445         0    3.1462         0    0.0000 ...
          0         0         0         0         0 ...
-   -0.2841         0    0.0259         0    0.0298 ...
+   -0.4403         0   -0.1888         0    0.0206 ...
          0         0         0         0         0 ...
-   -8.4105         0    1.8640         0    0.4307]; 
+   -6.2221         0   -3.3289         0    0.3500]; 
 
   
-lb = -40*ones(size(x0));
+lb = -100*ones(size(x0));
 lb(1) = t_min;
 % lb(2:6) = 
 lb(7:11) = zeros(1,5);
 lb(17:21) = zeros(1,5);
-ub =  40*ones(size(x0));
+ub =  100*ones(size(x0));
 ub(1) = t_max;
 ub(7:11) = zeros(1,5);
 ub(17:21) = zeros(1,5);
 
 %% run this optimization
-options = optimset('MaxFunEvals',10000);
-% options =[];
+% options = optimset('MaxFunEvals',10000);
+options =[];
 tic
 result = fmincon(@objective,x0,[],[],[],[],lb,ub,@mycon,options);
 toc
@@ -70,8 +70,7 @@ for j=1:length(timestamps)
 	burger_theta_ydot = [burger_theta_ydot , poly_derivative(ti,ae(5,:))];
 end
 figure(1000);
-% plot(timestamps,burger_x);
-% plot(timestamps,burger_y,timestamps,burger_z,timestamps,burger_theta_x);
+
 
 subplot(2,3,1);
 plot(timestamps,burger_x,timestamps,burger_z);
@@ -169,13 +168,13 @@ axis([0 0.4 -0.2 0.2 -0.1 0.3])
         % acceleration constraints are satisfied for 80% of the trajectory
           if i< 0.8 * length(time)  
               %acceleration
-            a_x = poly_double_derivative(t,a(1,:));
-            a_y = poly_double_derivative(t,a(2,:));
-            a_z = poly_double_derivative(t,a(3,:));
+%             a_x = poly_double_derivative(t,a(1,:));
+%             a_y = poly_double_derivative(t,a(2,:));
+%             a_z = poly_double_derivative(t,a(3,:));
           
             c = [c; 
-                 -(a_z*sin(theta_y)-a_x*cos(theta_y)+g*sin(theta_y)) ...
-                + mu* (a_x*sin(theta_y) + a_z*cos(theta_y) + g*cos(theta_y));
+%                  -(a_z*sin(theta_y)-a_x*cos(theta_y)+g*sin(theta_y)) ...
+%                 + mu* (a_x*sin(theta_y) + a_z*cos(theta_y) + g*cos(theta_y));
                 %                     spatulaedge_z - 0.20;
                 ];
           
@@ -191,23 +190,19 @@ axis([0 0.4 -0.2 0.2 -0.1 0.3])
 
         
         %% inequality terminal constraints
-        c  = [ c;
-                
-             ];
+%         c  = [ c;
+%                 
+%              ];
          
         
         %% equality terminal constraints
         
         ceq = [ceq;
                 theta_y_i - 0.35; % initial pitch angle
-                theta_y_f;         % final pitch angle
+                theta_y_f - 0.02;         % final pitch angle
 %                 thetadot_y_i;     % intitial pitch velocity
                 x_i;
-%                 y_i;
-%                 z_i;
                 x_f - 0.15;
-                z_f;
-%                 spatulaedge_z_i;
                 ];
                   
            
