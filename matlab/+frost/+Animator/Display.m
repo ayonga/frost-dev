27af51ca31e2a0924eb0b48463499b743729fdd9
Omar Harib <omar.harib@gmail.com>
2017-06-05 15:06:32 -0400

    Adding frost.Animator package

---
+++ matlab/+frost/+Animator/Display.m
@@ -0,0 +1,84 @@
+classdef Display < handle
+    % Creates a figure depicting a model
+    % 
+    % @author omar @date 2017-06-01
+    % 
+    % Copyright (c) 2017, UMICH Biped Lab
+    % All right reserved.
+    %
+    % Redistribution and use in source and binary forms, with or without
+    % modification, are permitted only in compliance with the BSD 3-Clause 
+    % license, see
+    % http://www.opensource.org/licenses/BSD-3-Clause
+    
+    properties (Access = protected)
+        model
+    end
+    
+    properties (Access = protected)
+        items
+    end
+    
+    properties (GetAccess = public, SetAccess = protected)
+        ax
+    end
+    
+    methods
+        function obj = Display(model, varargin)
+            f = figure;
+            obj.ax = axes(f);
+            obj.model = model;
+            
+            p = inputParser;
+            addParameter(p, 'UseExported', false);
+            addParameter(p, 'ExportPath', '');
+            addParameter(p, 'SkipExporting', false);
+            parse(p, varargin{:});
+            
+            obj.ax.DataAspectRatio = [1 1 1];
+            
+            hold(obj.ax, 'on');
+            
+            obj.items = containers.Map;
+            
+            for i = 1:length(obj.model.Joints)
+                name = ['Joint_', obj.model.Joints(i).Name];
+                obj.items(name) = frost.Animator.JointSphere(obj.ax, model, obj.model.Joints(i), name, varargin{:});
+            end
+            
+            for i = 1:length(obj.model.Links)
+                name = ['Link_', obj.model.Links(i).Name];
+                obj.items(name) = frost.Animator.LinkSphere(obj.ax, model, obj.model.Links(i), name, varargin{:});
+            end
+            
+            for i = 1:length(obj.model.Joints)
+                parentName = obj.model.Joints(i).Parent;
+                childName = obj.model.Joints(i).Child;
+                name = [parentName, '_to_', obj.model.Joints(i).Name];
+                obj.items(name) = frost.Animator.Lines(obj.ax, model, obj.model.Links(obj.model.getLinkIndices(parentName)), obj.model.Joints(i), name, varargin{:});
+                
+                name = [obj.model.Joints(i).Name, '_to_', childName];
+                obj.items(name) = frost.Animator.Lines(obj.ax, model, obj.model.Links(obj.model.getLinkIndices(childName)), obj.model.Joints(i), name, varargin{:});
+            end
+            
+            obj.update(zeros(length(obj.model.States.x), 1));
+        end
+        
+        function update(obj, x)
+            valueSet = obj.items.values();
+            for i = 1:length(valueSet)
+                valueSet{i}.update(x);
+            end
+        end
+        
+        function addItem(obj, item)
+            name = item.name;
+            obj.items(name) = item;
+        end
+        
+        function item = removeItem(obj, name)
+            item = obj.items(name);
+            obj.items.remove(name);
+        end
+    end
+end
---
+++ matlab/+frost/+Animator/Sphere.m
@@ -0,0 +1,76 @@
+classdef Sphere < frost.Animator.DisplayItem
+    % Sphere object for frost.Animator.Display
+    % 
+    % @author omar @date 2017-06-01
+    % 
+    % Copyright (c) 2017, UMICH Biped Lab
+    % All right reserved.
+    %
+    % Redistribution and use in source and binary forms, with or without
+    % modification, are permitted only in compliance with the BSD 3-Clause 
+    % license, see
+    % http://www.opensource.org/licenses/BSD-3-Clause
+    
+    properties (SetAccess = private, GetAccess = protected)
+        frame
+        surface
+    end
+    
+    properties (Access = public)
+        radius
+    end
+    
+    properties (GetAccess = public, SetAccess = private)
+        func_center
+    end
+    
+    methods
+        function obj = Sphere(ax, model, frame, name, varargin)
+            obj = obj@frost.Animator.DisplayItem(ax, model, name);
+            obj.frame = frame;
+            obj.radius = 1;
+            
+            p = inputParser;
+            addParameter(p, 'UseExported', false);
+            addParameter(p, 'ExportPath', '');
+            addParameter(p, 'SkipExporting', false);
+            parse(p, varargin{:});
+            
+            if p.Results.UseExported
+                if p.Results.SkipExporting == false
+                    expr = getCartesianPosition(obj.model, obj.frame).';
+                    symFunc = SymFunction([obj.name, '_sphere_center'], expr, {model.States.x});
+                    symFunc.export(p.Results.ExportPath);
+                end
+
+                obj.func_center = str2func([obj.name, '_sphere_center']);
+            else
+                expr = getCartesianPosition(obj.model, obj.frame).';
+                
+                obj.func_center = @(x) double(subs(expr, obj.model.States.x, x));
+            end
+            
+            x0 = zeros(length(obj.model.States.x), 1);
+            position = obj.func_center(x0);
+            
+            [xs, ys, zs] = sphere(10);
+            obj.surface = surf(obj.ax, xs*obj.radius + position(1),...
+                ys*obj.radius + position(2),...
+                zs*obj.radius + position(3),...
+                'FaceColor', 'black', 'EdgeColor', 'none');
+        end
+        
+        function obj = update(obj, x)
+            position = obj.func_center(x);
+            
+            [xs, ys, zs] = sphere(10);
+            obj.surface.XData = xs*obj.radius + position(1);
+            obj.surface.YData = ys*obj.radius + position(2);
+            obj.surface.ZData = zs*obj.radius + position(3);
+        end
+        
+        function obj = delete(obj)
+            delete(obj.surface);
+        end
+    end
+end
