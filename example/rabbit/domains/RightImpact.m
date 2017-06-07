1b46c1866c8fe6104be595751d0a6c7a12ea0c8a
Ross Hartley <m.ross.hartley@gmail.com>
2017-06-07 17:48:04 -0400

    Fixed rabbit example bug
    was constraining the wrong foot post impact, due to the automatic
    relabing of coordinates

--- example/rabbit/domains/RightImpact.m
+++ example/rabbit/domains/RightImpact.m
@@ -1,12 +1,12 @@
 % Right Foot Impact (guard)
-function guard = RightImpact(domain1, domain2)
+function guard = RightImpact(domain)
     
     % set impact
-    guard = RigidImpact('RightImpact',domain1,'leftFootHeight');
+    guard = RigidImpact('RightImpact',domain,'leftFootHeight');
     
     % Relabeling Matrix
     guard.R = guard.R(:,[1:3,6:7,4:5]);
     
     % set the impact constraint
-    guard.addImpactConstraint(struct2array(domain2.HolonomicConstraints));
+    guard.addImpactConstraint(struct2array(domain.HolonomicConstraints));
 end
