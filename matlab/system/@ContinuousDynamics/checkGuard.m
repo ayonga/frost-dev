0f9b8f0251b52c295663abfbc6777ed577d629fa
Ross_Hartley <m.ross.hartley@gmail.com>
2017-08-18 19:44:46 -0400

    Removed some code that wasn't inteded to be committed

--- matlab/system/@ContinuousDynamics/checkGuard.m
+++ matlab/system/@ContinuousDynamics/checkGuard.m
@@ -39,10 +39,6 @@ function [value, isterminal, direction] = checkGuard(obj, t, x, controller, para
         % compute the value of the event function
         value(i) = eventfuncs(i).calcConstraint(dep_val{:});
     end
-    
-    % Ross (hack) -- don't commit this!!!
-    if (controller.s < 0.1)
-        value(1) = 0;
-    end
+   
     
 end
